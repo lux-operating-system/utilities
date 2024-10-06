a75d04e6ba8ce80b402305ddd619bbb10145ccaa
jewelcodes <omarmelghoul01@gmail.com>
2024-10-06 02:02:46 -0400

    luxfetch: implemented a neofetch clone

--- luxfetch/luxfetch.c
+++ luxfetch/luxfetch.c
@@ -8,9 +8,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <fnctl.h>
+#include <sys/ioctl.h>
+#include <sys/lux/lux.h>
 
-#define COLOR       "\e[0;94m"
+#define COLOR       "\e[0;36m"
 #define RESET       "\e[0m"
+
+#define LFB_GET_WIDTH           (0x10 | IOCTL_OUT_PARAM)
+#define LFB_GET_HEIGHT          (0x20 | IOCTL_OUT_PARAM)
+
+char colors[73];
+
+char *colorTest(int bright) {
+    memset(colors, 0, 73);
+
+    for(int i = 0; i < 8; i++) {
+        if(bright) sprintf(colors+strlen(colors), "\e[10%dm   ", i);
+        else sprintf(colors+strlen(colors), "\e[4%dm   ", i);
+    }
+
+    return colors;
+}
 
 int main(void) {
     // get kernel info
@@ -37,6 +57,98 @@ int main(void) {
         return -1;
     }
 
-    printf("%s\n", kernel);
+    // read uptime
+    fclose(f);
+    f = fopen("/proc/uptime", "r");
+    if(!f) {
+        fprintf(stderr, "unable to open /proc/uptime for reading\n");
+        free(kernel);
+        return -1;
+    }
+
+    uint64_t uptime = 0;
+    fread(&uptime, 1, 8, f);
+
+    uptime /= 1000;     // convert to seconds
+
+    // read memory usage
+    uint32_t memusage = 0, memsize = 0;
+    f = fopen("/proc/memsize", "r");
+    if(!f) {
+        fprintf(stderr, "unable to open /proc/memsize for reading\n");
+        free(kernel);
+        return -1;
+    }
+
+    fread(&memsize, 1, 4, f);
+    fclose(f);
+
+    f = fopen("/proc/memusage", "r");
+    if(!f) {
+        fprintf(stderr, "unable to open /proc/memusage for reading\n");
+        free(kernel);
+        return -1;
+    }
+
+    fread(&memusage, 1, 4, f);
+    fclose(f);
+
+    // get screen resolution
+    unsigned long width, height;
+    int fd = open("/dev/lfb0", O_RDONLY);
+    if(fd < 0) {
+        free(kernel);
+        fprintf(stderr, "unable to open /dev/lfb0 for reading\n");
+        return -1;
+    }
+
+    if(ioctl(fd, LFB_GET_WIDTH, &width)) {
+        close(fd);
+        free(kernel);
+        fprintf(stderr, "unable to get screen resolution\n");
+        return -1;
+    }
+
+    if(ioctl(fd, LFB_GET_HEIGHT, &height)) {
+        close(fd);
+        free(kernel);
+        fprintf(stderr, "unable to get screen resolution\n");
+        return -1;
+    }
+
+    close(fd);
+
+    // more readability
+    int hours = (uptime / 3600);
+    int mins = (uptime / 60) % 60;
+    int secs = (uptime % 60);
+
+    int usageMB = memusage / 256;
+    int sizeMB = memsize / 256;
+
+    printf(COLOR "          @@@@@@@@         %s" RESET "@" COLOR "localhost" RESET "\n", "root");
+    printf(COLOR "        @@@      @@@       " RESET "--------------\n");
+    printf(COLOR "      @@            @@     OS: " RESET "luxOS development\n");
+    printf(COLOR "     @@              @@    Kernel: " RESET "%s\n", kernel);
+    printf(COLOR "     @@              @@    Uptime: " RESET);
+
+    if(hours) {
+        printf("%d hours, %d mins, %d mins\n", hours, mins, secs);
+    } else if(mins) {
+        printf("%d mins, %d secs\n", mins, secs);
+    } else {
+        printf("%d secs\n", secs);
+    }
+
+    printf(COLOR "     @@              @@    Shell: " RESET "%s\n", getenv("SHELL"));
+    printf(COLOR "     @@              @@    Resolution: " RESET "%dx%d\n", (int) width, (int) height);
+    printf(COLOR "      @@            @@     Terminal: " RESET "%s\n", ttyname(0));
+    printf(COLOR "       @@@        @@@      CPU: " RESET "\n");
+    printf(COLOR "         @@@    @@@        Memory: " RESET "%d MiB / %d MiB\n", usageMB, sizeMB);
+    printf(COLOR "          @@@@@@@@         " RESET "\n");
+    printf(COLOR "                           " RESET "%s\n", colorTest(0));
+    printf(COLOR "          @@@@@@@@         " RESET "%s\n", colorTest(1));
+    printf("\n");
+
     return 0;
 }
