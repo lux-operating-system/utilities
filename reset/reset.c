bed262eb53c602e1a928e6b9399d09eadd1491d5
jewelcodes <omarmelghoul01@gmail.com>
2025-01-12 19:56:54 -0500

    reset: enable ISIG by default and log interrupt character if changed

--- reset/reset.c
+++ reset/reset.c
@@ -6,15 +6,16 @@
  */
 
 #include <stdio.h>
+#include <ctype.h>
 #include <stdlib.h>
 #include <termios.h>
 
 #define DEFAULT_IFLAG           (ICRNL | IGNCR | IGNPAR)
 #define DEFAULT_OFLAG           (ONLRET)
 #define DEFAULT_CFLAG           (CS8 | HUPCL)
-#define DEFAULT_LFLAG           (ECHO | ECHOE | ECHOK | ECHONL | ICANON)
+#define DEFAULT_LFLAG           (ECHO | ECHOE | ECHOK | ECHONL | ICANON | ISIG)
 
-int main(void) {
+int main(int argc, char **argv) {
     struct termios termios;
     if(tcgetattr(fileno(stdin), &termios)) return EXIT_FAILURE;
 
@@ -25,6 +26,9 @@ int main(void) {
     if(tcsetattr(fileno(stdin), TCSANOW, &termios)) return EXIT_FAILURE;
 
     printf("\e[0m\e[2J\e[1;1H");
-    fflush(stdout);
+    if(isprint(termios.c_cc[VINTR])) {
+        printf("%s: interrupt character is '%c'\n", argv[0], (char) termios.c_cc[VINTR]);
+    }
+
     return EXIT_SUCCESS;
 }
